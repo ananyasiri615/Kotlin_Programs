1b0e1c0e35e65d99c8a9eefe1d3e6ecd41043ec2
Ananya R Siri <ananyasiri615@gmail.com>
2023-07-13 11:18:31 +0530

    1

---
+++ src/main/kotlin/day_four/MultipleInterfaces.kt
@@ -0,0 +1,36 @@
+package day_four
+
+interface A {
+
+    fun display(){
+        println("Display function of A interface")
+    }
+}
+
+interface B {
+
+    fun display(){
+        println("Display function of B interface")
+    }
+
+    fun displayDefault(){
+        println("Display default function")
+    }
+}
+
+class ABImpl : A, B {
+    override fun display() {
+        //super<A>.display()
+        println("Display function of ABIMPL")
+    }
+}
+
+fun main(args: Array<String>) {
+
+    val obj = ABImpl()
+
+    obj.display()
+
+    obj.displayDefault()
+}
+
